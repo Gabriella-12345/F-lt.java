2d680aac6f37f0cb648339412da9f3363c27dee3
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2023-09-25 09:06:05 +0200

    skapa en array med 4 siffror, uppgift a

--- src/App.java
+++ src/App.java
@@ -1,6 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Fält");
-        
+        int []siff = new int[4];
+        siff[0] = 8;
+        siff[1] = 4;
+        siff[2] = 6;
+        siff[3] = 3;
+        siff[4] = 9;
+        System.out.println("siffera 1:" + siff [0]);
+        System.out.println("siffera 2" + siff[1]);
+        System.out.println("siffera 3" + siff[2]);
+        System.out.println("siffera 4" + siff[3]);
+        // int celler = siff.length
     }
 }
