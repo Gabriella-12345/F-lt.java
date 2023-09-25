a19c39f1715c6ff5ca3488de77172f8ca5bba15a
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2023-09-25 09:15:17 +0200

    ln 15 berättar längden för fält 1

--- src/App.java
+++ src/App.java
@@ -1,16 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Fält");
-        int []siff = new int[4];
+        System.out.println("Fält_1");
+        int []siff = new int[5];
         siff[0] = 8;
         siff[1] = 4;
         siff[2] = 6;
         siff[3] = 3;
         siff[4] = 9;
-        System.out.println("siffera 1:" + siff [0]);
+        System.out.println("siffera 1" + siff[0]);
         System.out.println("siffera 2" + siff[1]);
         System.out.println("siffera 3" + siff[2]);
         System.out.println("siffera 4" + siff[3]);
-        // int celler = siff.length
+        System.out.println("siffera 4" + siff[4]);
+        System.out.println("Antal element i siff = "+ siff.length);
     }
 }
