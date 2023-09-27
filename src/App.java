b54aa5b2361e5f398b8f65d3c24f4ada0f7a71e2
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2023-09-27 10:12:38 +0200

    skapa ett fjärde array

--- src/App.java
+++ src/App.java
@@ -15,9 +15,9 @@ public class App {
         System.out.println("siffera 4" + siff[4]);
 
         System.out.println("Antal element i siff = "+ siff.length);
-        System.out.println((siff[0] + siff[1] + siff[2]+ siff[3] + siff[4])/siff.length); 
+        System.out.println(( + siff[0] + siff[1] + siff[2]+ siff[3] + siff[4])/siff.length); 
 
-        System.out.println("Fält 2");
+        System.out.println("Fält_2");
         int []sif = new int [5];
 
         sif[0] = 18;
@@ -25,11 +25,29 @@ public class App {
         sif[2] = 16;
         sif[3] = 13;
         sif[4] = 19; 
-        
+
         System.out.println("siffera 1" + sif[0]);
         System.out.println("siffera 2" + sif[1]);
         System.out.println("siffera 3" + sif[2]);
         System.out.println("siffera 4" + sif[3]);
         System.out.println("siffera 4" + sif[4]);
+
+        System.out.println("Antal element i sif = "+ sif.length);
+        System.out.println(( + sif[0] + sif[1] + sif[2]+ sif[3] + sif[4])/sif.length);
+
+        System.out.println(siff[] + sif[]);
+
+        int []fyra = new int[4];
+        siff[0] = 1;
+        siff[1] = 1;
+        siff[2] = 1;
+        siff[3] = 1;
+
+        System.out.println("siffera 1" + siff[0]);
+        System.out.println("siffera 2" + siff[1]);
+        System.out.println("siffera 3" + siff[2]);
+        System.out.println("siffera 4" + siff[3]);
+
+        
     }
 }
